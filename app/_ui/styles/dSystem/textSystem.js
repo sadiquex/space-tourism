ffe7c9c0f3341ce29933f580e7e207bf5b8e099d
sadiquex <abubakasaddik1@gmail.com>
2023-11-05 18:42:09 +0000

    setup routing and design system

---
+++ app/_ui/styles/dSystem/textSystem.js
@@ -0,0 +1,49 @@
+"use client";
+import styled from "styled-components";
+/* 
+This file defines all the text styles
+*/
+
+// titles
+export const H1 = styled.h1`
+  font-family: var(--font-bellefair), sans-serif;
+  font-size: 150px;
+`;
+
+export const H2 = styled.h2`
+  font-family: var(--font-bellefair), sans-serif;
+  font-size: 100px;
+`;
+
+export const H3 = styled.h3`
+  font-family: var(--font-bellefair), sans-serif;
+  font-size: 56px;
+`;
+
+export const H4 = styled.h4`
+  font-family: var(--font-bellefair), sans-serif;
+  font-size: 32px;
+`;
+
+export const H5 = styled.h5`
+  font-family: var(--font-barlow-condensed), sans-serif;
+  font-size: 28px;
+  letter-spacing: 4.75;
+`;
+
+export const SubHeading_1 = styled.p`
+  font-family: var(--font-bellefair);
+`;
+
+export const SubHeading_2 = styled.p`
+  font-family: var(--font-barlow-condensed), sans-serif;
+  font-size: 14px;
+  letter-spacing: 2.35;
+  color: #d0d6f9;
+`;
+
+export const Nav_Text = styled.p`
+  font-family: var(--font-barlow-condensed), sans-serif;
+  font-size: 16px;
+  letter-spacing: 2.7;
+`;
